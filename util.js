8de03c74f36d68102f8c5e0d503f5244ac65de15
roman-bg <61371346+roman-bg@users.noreply.github.com>
2021-12-13 17:53:43 +0200

    Update util.js

--- util.js
+++ util.js
@@ -1,6 +1,7 @@
 var mqtt = { hostname: "co2.quest", port: 9393 };
 var topic_name = "vt/dd";
 var topic1_name = "sf/dd";
+var topic2_name = "pl/dd";
 var publish_cnt = 0;
 var dataTopics = new Array();
 // called when the client loses its connection
@@ -16,9 +17,13 @@ demo.innerHTML = " Велико Търново: "+message.payloadString;
 
   console.log(" Велико Търново "+ message.payloadString);
   
-  if(message.destinationName == "sf/dd")
+    if(message.destinationName == "sf/dd")
       demo1.innerHTML = " София: "+ message.payloadString;
-      console.log(" SF "+ message.payloadString);
+      console.log(" София "+ message.payloadString);
+  
+     if(message.destinationName == "pl/dd")
+      demo2.innerHTML = " Пловдив: "+ message.payloadString;
+      console.log(" Пловдив "+ message.payloadString)
 }
 
 
